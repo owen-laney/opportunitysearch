e6f46368276089367bce0e9bde51fe576e4745ab
Owen <97751860+owen-laney@users.noreply.github.com>
2024-06-21 18:13:57 -0400

    Polish sidebar and add items

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -1,3 +1,11 @@
-<div class="bg-green-400 flex w-64 h-screen flex-shrink-0">
-    hi!
+<script lang="ts">
+    import Sidebaritem from "$lib/components/sidebaritem.svelte";
+</script>
+
+<div class="bg-stone-100 flex w-64 h-screen flex-shrink-0 p-2 flex-col">
+    <Sidebaritem text="Scholarships" url="/scholarships"/>
+    <Sidebaritem text="Fly-In Programs" url="/flyin"/>
+    <Sidebaritem text="Organizations" url="/organizations"/>
+    <Sidebaritem text="Testing" url="/tests"/>
+    <Sidebaritem text="Dual Enrollment" url="/dualenrollment"/>
 </div>
---
+++ src/lib/components/sidebaritem.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let url = 'https://example.com';
+	export let text = 'Link Name';
+	export let style = 'regular';
+	import { page, navigating } from '$app/stores';
+</script>
+
+<a href={url} aria-current={$page.url.pathname === url} class="text-sm rounded-lg px-3 py-2 hover:bg-stone-200 h-min w-full active:scale-95" >
+    {text}
+</a>
